460ee0ac2426e10accdd69c6306835f511a7ccfc
navyathatikonda <43335840+navyathatikonda@users.noreply.github.com>
2018-09-18 13:02:50 +0530

    add files

---
+++ social/Mail.java
@@ -0,0 +1,31 @@
+package mydatatypes.social;
+
+public class Mail {
+String mail_type;
+String mail_id;
+int mail_send;
+int mail_receive;
+public void compose_mail()
+{
+	
+}
+public void receive_mail()
+{
+	
+}
+public void delete_mail()
+{
+	
+}
+public void draft_mail()
+{
+	
+}
+public Mail(String mail_type,String mail_id,int mail_send,int mail_receive)
+{
+	this.mail_type=mail_type;
+	 this.mail_id=mail_id;
+	 this.mail_send= mail_send;
+	 this.mail_receive=mail_receive;
+}
+}
---
+++ social/WattsappGroup.java
@@ -0,0 +1,30 @@
+package mydatatypes.social;
+
+public class WattsappGroup {
+	String grp_name;
+	int participants_No;
+	String participants_name;
+public void add_participates() {
+	
+}
+public void remove_participates() {
+	
+}
+public void send_msg()
+{
+	
+}
+public void receive_msg() 
+{
+	
+}
+public void exit_grp()
+{
+	
+}
+public WattsappGroup(String grp_name,int participants_No,String participants_name) {
+this.grp_name =grp_name;
+this.participants_No=participants_No;
+this.participants_name=participants_name;
+}
+}
